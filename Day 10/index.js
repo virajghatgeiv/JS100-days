22f2cc7a24a137e8291c9f819b34d96a37503cce
virajghatgeiv <85924082+virajghatgeiv@users.noreply.github.com>
2022-02-03 01:15:47 +0530

    Add files via upload

---
+++ Day 10/index.js
@@ -0,0 +1,22 @@
+/*pseudocode for binary tree
+
+**node class creation**
+create node class which will have current value, left child and right child
+
+
+**Binary Tree class creation with add method**
+{creat binary tree class which will have root 
+
+need to creat method to add node to binary tree
+
+ {check if root value is null
+  then assign directly given value to root
+ otherwise check given value is less than root value
+  then check if left child value is null
+   then assign given value directly to left child
+   otherwise call add function again by passing root value as left child and given value
+ otherwise check if given value is greater than root value
+  then check if right child value is null
+   then assign given value directly to right child
+  otherwise call add function again by passing root value as right child and given value}
+}*/
