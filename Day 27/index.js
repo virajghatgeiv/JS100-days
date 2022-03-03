4e47f5947b6d16688c3c6e19eb1a5850439b85e6
virajghatgeiv <85924082+virajghatgeiv@users.noreply.github.com>
2022-03-03 21:44:33 +0530

    Add files via upload

---
+++ Day 27/index.js
@@ -0,0 +1,49 @@
+const undirctedPath = (edges,nodeA,nodeB) => {
+  const graph = convertGraph(edges)
+  return hasPath(graph,nodeA,nodeB, new Set())
+}
+
+const hasPath = (graph,src,dst,visited) => {
+ if(src == dst){
+  return true
+ }
+ if(visited.has(src)){
+  return false
+ }
+ visited.add(src)
+ for(let neighbour of graph[src]){
+  if(hasPath(graph,neighbour,dst,visited) == true){
+   return true
+  }
+ }
+ return false
+}
+
+const convertGraph = (edges) => {
+  const graph = {}
+
+  for(let edge of edges){
+   const [a,b] = edge
+   if(!(a in graph)){
+    graph[a] = []
+   }
+   if(!(b in graph)){
+    graph[b] = []
+   }
+   graph[a].push(b)
+   graph[b].push(a)
+  }
+  return graph
+}
+
+
+const edges= [
+ ['i','j'],
+ ['k','i'],
+ ['m','k'],
+ ['k','l'],
+ ['o','n']
+]
+
+
+console.log(undirctedPath(edges,'i','l'))
